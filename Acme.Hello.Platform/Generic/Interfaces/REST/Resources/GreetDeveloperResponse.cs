3c8ccfbaa881c116c49384834dc0ac872afbf834
Ro <u202213765@upc.edu.pe>
2025-04-09 12:49:40 -0500

    feat(greeting): add developer response resource.

---
+++ Acme.Hello.Platform/Generic/Interfaces/REST/Resources/GreetDeveloperResponse.cs
@@ -0,0 +1,10 @@
+namespace Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
+public record GreetDeveloperResponse(Guid? Id, string? FullName, string Message)
+{
+    public GreetDeveloperResponse(string Message) : this(null, null, Message) 
+    {
+        
+    }
+};
+
